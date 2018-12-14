61ee0c91ae547e181e2d73c2f8e1cbb756ed3601
loefflerj <loefflju@informatik.uni-freiburg.de>
2018-12-14 15:31:41 +0100

    cmd line args, multidirectory processing

--- needleman_wunsch.py
+++ needleman_wunsch.py
@@ -1,13 +1,29 @@
-import logging
 from pprint import pformat
 
 import numpy
 from Bio.SubsMat import MatrixInfo
 
 from logger.log import setup_custom_logger
-from utility.utils import Alignment, Operation, ScoringType, parse_fasta_files
+from utility.utils import Alignment, Operation, ScoringType, check_for_duplicates, parse_directory, parse_fasta_files, \
+    split_directories_and_files
 
-LOGGER = setup_custom_logger("needleman_wunsch", logfile="logfile.log")
+LOGGER = setup_custom_logger("nw", logfile="needleman_wunsch.log")
+
+import argparse
+import os
+import itertools
+
+# command line arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', type=str, required=True, default='',
+                    nargs='+',
+                    help='A file, a directory or multiple directories. directories are processed recursively.')
+parser.add_argument('--file-filter', type=str, default='',
+                    help='A regex to define a filter, which will be applied to the files parsed.')
+parser.add_argument('-a', '--all', action='store_true', default=False,
+                    help='Return ALL optimal alignments.')
+
+args = parser.parse_args()
 
 
 class TracebackCell(object):
@@ -26,6 +42,16 @@ class TracebackCell(object):
 
     def __repr__(self):
         return "(%s, %s)" % (self.predecessors, self.score)
+
+
+class Result(object):
+    def __init__(self, seq1_ID, seq1, seq2_ID, seq2, alignments, score):
+        self.seq1_ID = seq1_ID
+        self.seq2_ID = seq2_ID
+        self.seq1 = seq1
+        self.seq2 = seq2
+        self.alignments = alignments
+        self.score = score
 
 
 class NeedlemanWunsch(object):
@@ -201,15 +227,19 @@ class NeedlemanWunsch(object):
         self.tracebacks = self.explore(self.desired_traceback, [])
         LOGGER.info("Done.")
 
-    def generate_alignments(self, sequence1, sequence2):
+    def generate_alignments(self, sequence1, sequence2, all=False):
         """
         Function which creates all optimal alignments.
         :param sequence1:
         :param sequence2:
         :return:
         """
+        LOGGER.info("Generating Alignments.")
         alignments = []
         for traceback in self.tracebacks:
+            if not all and len(alignments) >= 1:
+                self.alignments = alignments
+                return
             LOGGER.info("Length of tb: %d" % len(traceback))
             LOGGER.info("tb: %s" % traceback)
             seq1 = ""
@@ -253,7 +283,7 @@ class NeedlemanWunsch(object):
         self.calculate_scoring_matrix(seq1.seq, seq2.seq)
         self.desired_traceback = self.traceback_matrix[-1][-1]
         self.split_traceback_set()
-        self.generate_alignments(sequence1=seq1.seq, sequence2=seq2.seq)
+        self.generate_alignments(sequence1=seq1.seq, sequence2=seq2.seq, all=complete_traceback)
 
         res = None
         if self.alignments:
@@ -263,26 +293,73 @@ class NeedlemanWunsch(object):
                 res = self.alignments
 
         # return some dummy results
-        return (seq1.id,
-                seq1.seq,
-                seq2.id,
-                seq2.seq,
-                self.desired_traceback.score,
-                res)
+        return Result(seq1_ID=seq1.id,
+                      seq1=seq1.seq,
+                      seq2_ID=seq2.id,
+                      seq2=seq2.seq,
+                      score=self.desired_traceback.score,
+                      alignments=res)
+
+
+def parse_input():
+    fasta_files = []
+    # split input into files and directories.
+    directories, files = split_directories_and_files(input_list=args.input)
+    # check if input files are part of the directories to be checked
+    # an  check if directories are subdirectories of other directories.
+    directories, files = check_for_duplicates(directories=directories, files=files)
+    for file in files:
+        fasta_files.append(file)
+        # process directories and get fastafiles.
+    for dir_name in directories:
+        directory_content = parse_directory(dir_name, file_filter=args.file_filter)
+        for entry in directory_content:
+            os.chdir(entry["directory"])
+            if entry["files"] != [] and entry["directory"] != '':
+                fasta_files.extend(entry["files"])
+    LOGGER.info("Collected the following fasta files:\n %s" % pformat(fasta_files))
+    sequences = parse_fasta_files(fasta_files)
+    LOGGER.info("Parsed the following sequences:\n %s" % pformat(sequences))
+    return sequences
+
+
+def process_program_arguments():
+    if not args.input:
+        LOGGER.critical(
+                "Error, provide input file/files/directory/directories using -i / --input. -h/--help for all "
+                "arguments.")
+        exit(1)
+
+
+def run_needleman():
+    sequences = parse_input()
+    if len(sequences) < 2:
+        LOGGER.warn("We received not enough sequences. Make sure you called the program correctly.")
+        exit(1)
+    elif len(sequences) >= 2:
+        # init the needleman
+        nw = NeedlemanWunsch()
+        LOGGER.info("You provided a total of %d sequences, performing pairwise aligment." % len(sequences))
+        combinations = list(itertools.combinations(sequences, 2))
+        total = len(combinations)
+        current = 1
+        for x, y in combinations:
+            LOGGER.info(" Alignment %d / %d (SEQ1: %s, SEQ2: %s)" % (current, total, x.seq, y.seq))
+            res = nw.run(x, y, complete_traceback=args.all)
+            LOGGER.info("SEQUENCE PAIR:\nSEQ1 ID:%s SEQ:%s\n"
+                        "SEQ2 ID:%s SEQ:%s" % (x.id, x.seq, y.id, y.seq))
+            LOGGER.info("SCORE: %s" % res.score)
+            LOGGER.info("PRINTING ALIGNMENT(S): ")
+            for alignment in res.alignments:
+                LOGGER.info("ALIGNMENT\n%s\n%s" % (alignment.sequence1, alignment.sequence2))
+                print()
+
+
+def main():
+    process_program_arguments()
+    run_needleman()
+    exit(1)
 
 
 if __name__ == '__main__':
-    # run Needleman-Wunsch with some parameters
-    nw = NeedlemanWunsch()
-    fasta_files = ["data/test3.fa"]
-    sequences = parse_fasta_files(fasta_files)
-    seq1 = sequences[0]
-    seq2 = sequences[1]
-    res = nw.run(seq1, seq2)
-    if LOGGER.level == logging.DEBUG:
-        LOGGER.debug("scoring:", nw.scoring_matrix)
-        LOGGER.debug("Traceback-matrix:\n %s" % nw.traceback_matrix)
-        LOGGER.debug("Traceback:\n %s" % nw.desired_traceback)
-        LOGGER.debug("Done.")
-    print(res)
-    exit(0)
+    main()
--- utility/utils.py
+++ utility/utils.py
@@ -1,3 +1,6 @@
+import fnmatch
+import os
+import re
 from enum import Enum
 
 from Bio import SeqIO
@@ -29,7 +32,7 @@ class Alignment(object):
 
 def parse_fasta_files(files):
     """
-    >>> files = [test1.fa,test1.faa/test2.fn"]
+    >>> files = []
     >>> parse_fasta_files(files)
     [SeqRecord(seq=Seq('AAAA', SingleLetterAlphabet()), id='test1', name='test1', description=' test1', dbxrefs=[]), \
 SeqRecord(seq=Seq('AAAA', SingleLetterAlphabet()), id='test2', name='test2', description='test2', dbxrefs=[])]
@@ -42,3 +45,108 @@ SeqRecord(seq=Seq('AAAA', SingleLetterAlphabet()), id='test2', name='test2', des
     for file in files:
         records.extend(list(SeqIO.parse(file, "fasta")))
     return records
+
+
+def parse_directory(dir_path, file_filter=''):
+    """
+    Function which will parse a directory recursively and return a list of dictionaries.
+    They are of the form {"directory" : <dirpath>, "subdirs" : <dirnames>, "files" : <filenames>}.
+    """
+    directory_content = []
+    for (dir_path, dir_names, file_names) in os.walk(dir_path):
+        # filter out any files which do not end with .c
+        dir_path = os.path.abspath(dir_path)
+        filtered_filenames = [os.path.join(dir_path, fi) for fi in file_names if (
+            matches_file_filter(fi, file_filter) if file_filter else True)]
+        content = {"directory": dir_path, "subdirs": dir_names, "files": filtered_filenames}
+        directory_content.append(content)
+    return directory_content
+
+
+def split_directories_and_files(input_list):
+    """
+    Function which takes a list of directories/files and splits them up
+    :param input_list:
+    :return: 2 lists, directories and files.
+    >>> input_list = ["../testdir","../testdir/testsubdir1/safeExample.c"]
+    >>> split_directories_and_files(input_list)
+    (['../testdir'], ['../testdir/testsubdir1/safeExample.c'])
+    """
+    directories = []
+    files = []
+    for item in input_list:
+        if os.path.isfile(item):
+            files.append(item)
+        elif os.path.isdir(item):
+            directories.append(item)
+    return directories, files
+
+
+def check_for_duplicates(directories, files):
+    """
+    Function that removes redundant paths from files,directories.
+    example:
+    1. /this/is/a/path/
+    2. /this/is/a/path/children
+    2 is a child of 1 and will be removed.
+    :param directories:
+    :param files:
+    :return: directories,files lists, without redundant files
+    >>> directories = ["../testdir",]
+    >>> files = ["../testdir/testsubdir1/safeExample.c"]
+    >>> check_for_duplicates(directories,files)
+    (['../testdir'], [])
+    """
+    # check if files are in a directory.
+    for dir_name in directories:
+        for file in files:
+            if file.startswith(dir_name):
+                files.remove(file)
+                continue
+    # check if directories are subdirectories.
+    for dir_name in directories:
+        tmp_dirs = directories.copy()
+        tmp_dirs.remove(dir_name)
+        for x in tmp_dirs:
+            if dir_name.startswith(x):
+                directories.remove(dir_name)
+    return directories, files
+
+
+def matches_file_filter(filename, file_filter=''):
+    """
+    Function which checks whether a function matches the file-filter argument (regex)
+    :param filename:
+    :param file_filter:
+    :return: true if match.
+
+    >>> matches_file_filter("some/path/req_bl_2000", "req_bl*")
+    True
+    >>> matches_file_filter("req_bl_2000", "*_bl*")
+    True
+    >>> matches_file_filter("req_bl_2000.c.TESTMASTER.c", "*_bl*.TESTMASTER*")
+    True
+    >>> matches_file_filter("req_bl_2000", "nomatch*")
+    False
+    """
+    if file_filter:
+        match = fnmatch.fnmatch(os.path.basename(filename), file_filter)
+        if match:
+            return True
+        else:
+            return False
+    else:
+        return True
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [atoi(c) for c in re.split('(\d+)', text)]
